13c74af0d583e6c5842e5e98315a95112b163909
fuuu555 <joeph20050505@gmail.com>
2024-12-16 17:43:11 +0800

    firebase

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ dotenv.config();
 
 const app = express();
 const __dirname = path.resolve();
-
+app.use(express.json());  
 app.use(cors());
+app.use('/firebase.js', express.static(path.join(__dirname, 'firebase.js')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', router);
 app.use((req, res) => {
